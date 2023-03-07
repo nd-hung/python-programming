856e238ac24d959f970c5089cf30817358c2d7fd
Hung Nguyen <hungdhnt@gmail.com>
2023-03-07 08:10:46 +0700

    Fixed code format

--- docs/labs/01-dotnet-and-csharp/electricitybill/Program.cs
+++ docs/labs/01-dotnet-and-csharp/electricitybill/Program.cs
@@ -17,29 +17,29 @@ namespace TinhTienDien
     {
         // Khai báo các hằng 
         // Các mức tiêu thụ (kWh)
-        const int Muc1=100, Muc2=150, Muc3=300;
+        const int Muc1 = 100, Muc2 = 150, Muc3 = 300;
         // Các mức giá (VND/kWh)
-        const int Gia1=2000, Gia2=2500, Gia3=3000;
+        const int Gia1 = 2000, Gia2 = 2500, Gia3 = 3000;
 
         static void Main()
         {
-            int sokWh=0;
-            double sotien=0;
+            int sokWh = 0;
+            double sotien = 0;
 
             // Nhập số kWh tiêu thụ 
             Console.Write("Nhap so kWh tieu thu:");
             sokWh = int.Parse(Console.ReadLine());
-        
+
             // Tính tiền điện dựa vào số kWh tiêu thụ và Bảng giá
-            if(sokWh<=0) sotien=0;
-            else if (sokWh<=Muc1) sotien = sokWh*Gia1;
-            else if(sokWh<=Muc2) sotien = Muc1*Gia1 + (sokWh-Muc1)*Gia2;
-            else if(sokWh<=Muc3) sotien = Muc1*Gia1 + Muc2*Gia2+(sokWh-Muc2)*Gia3;
+            if (sokWh <= 0) sotien = 0;
+            else if (sokWh <= Muc1) sotien = sokWh * Gia1;
+            else if (sokWh <= Muc2) sotien = Muc1 * Gia1 + (sokWh - Muc1) * Gia2;
+            else if (sokWh <= Muc3) sotien = Muc1 * Gia1 + Muc2 * Gia2 + (sokWh - Muc2) * Gia3;
             // Nếu tiêu thụ quá 300 thì cộng thêm 10%
-            if(sokWh>300) sotien = Muc1*Gia1 + Muc2*Gia2+(sokWh-Muc2)*Gia3*1.1;
+            if (sokWh > 300) sotien = Muc1 * Gia1 + Muc2 * Gia2 + (sokWh - Muc2) * Gia3 * 1.1;
 
             // Output
-            Console.WriteLine("So kWh tieu thu: {0}, so tien: {1}\n",sokWh, sotien);
+            Console.WriteLine("So kWh tieu thu: {0}, so tien: {1}\n", sokWh, sotien);
         }
     }
 }
