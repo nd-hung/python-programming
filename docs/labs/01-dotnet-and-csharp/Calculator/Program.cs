92a78258bd4de5ae0874dacea77d2250d5e2a024
Hung Nguyen <hungdhnt@gmail.com>
2023-03-08 20:16:59 +0700

    Update

--- docs/labs/01-dotnet-and-csharp/Calculator/Program.cs
+++ docs/labs/01-dotnet-and-csharp/Calculator/Program.cs
@@ -1,13 +1,4 @@
-﻿/*
-Viết chương trình nhập từ bàn phím hai số nguyên a, b và một ký tự ch.
-Nếu:
-- ch là "+" thì thực hiện phép tính a + b và in kết quả lên màn hình.
-- ch là "–" thì thực hiện phép tính a - b và in kết quả lên màn hình.
-- ch là "*" thì thực hiện phép tính a * b và in kết quả lên màn hình.
-- ch là "/" thì thực hiện phép tính a / b và in kết quả lên màn hình. Lưu ý kiểm tra điều kiện của phép chia.
-*/
-
-namespace Calculator
+﻿namespace Calculator
 {
     internal class Program
     {
