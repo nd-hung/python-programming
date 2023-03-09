bceb0ac4c4ced201b4394171b84381cafec0367e
nd-hung <hungdhnt@gmail.com>
2023-03-09 09:48:01 +0700

    Fixed calculator app

--- docs/labs/01-dotnet-and-csharp/Calculator/Program.cs
+++ docs/labs/01-dotnet-and-csharp/Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace SimpleCalculator
         public static void Main()
         {
             int a, b;   // Khai báo 2 biến chứa giá trị đọc từ bàn phím
-            bool readNumber1OK=false, readNumber2OK=false; // Dùng để kiểm tra giá trị đọc vào 
+            bool readNumber1OK = false, readNumber2OK = false; // Dùng để kiểm tra giá trị đọc vào 
 
             // Lặp lại việc đọc số thứ nhất cho đến khi giá trị hợp lệ
             do
@@ -16,32 +16,31 @@ namespace SimpleCalculator
             } while (!readNumber1OK);
 
             // Lặp lại việc đọc số thứ hai đến khi giá trị hợp lệ
-            do {
+            do
+            {
                 Console.Write("Nhap so thu hai = ");
                 readNumber2OK = int.TryParse(Console.ReadLine(), out b);
-            }while(!readNumber2OK);
+            } while (!readNumber2OK);
 
             // Đọc ký hiệu phép toán (đọc 1 ký tự)
             Console.Write("Nhap ky hieu phep toan (+,-,*,/) = ");
             char op = Console.ReadKey().KeyChar;
 
-            // Nếu cả 2 số nhập hợp lệ -> kiểm tra phép toán và thực hiện  
-            if (readNumber1OK && readNumber2OK)
+            // Kiểm tra phép toán và tính biểu thức tương ứng
+            switch (op)
             {
-                switch (op)
-                {
-                    case '+': Console.WriteLine("\n{0} + {1} = {2}", a, b, a + b); break;
-                    case '-': Console.WriteLine("\n{0} - {1} = {2}", a, b, a - b); break;
-                    case '*': Console.WriteLine("\n{0} * {1} = {2}", a, b, a * b); break;
-                    case '/':
-                        if (b == 0)
-                            Console.WriteLine("\nKhong chia duoc!");
-                        else
-                            Console.WriteLine("\n{0}/{1} = {2}", a, b, (double)a / b);
-                        break;
-                    default: Console.WriteLine("\nKhong biet phep toan!"); break;
-                }
+                case '+': Console.WriteLine("\n{0} + {1} = {2}", a, b, a + b); break;
+                case '-': Console.WriteLine("\n{0} - {1} = {2}", a, b, a - b); break;
+                case '*': Console.WriteLine("\n{0} * {1} = {2}", a, b, a * b); break;
+                case '/':
+                    if (b == 0)
+                        Console.WriteLine("\nKhong chia duoc!");
+                    else
+                        Console.WriteLine("\n{0}/{1} = {2}", a, b, (double)a / b);
+                    break;
+                default: Console.WriteLine("\nKhong biet phep toan!"); break;
             }
+
         }
     }
 }
