79b1e04f3ac782968d531dc849687e91bea84909
Hung Nguyen <hungdhnt@gmail.com>
2023-03-14 20:32:02 +0700

    Update examples

---
+++ docs/labs/02-classes-and-objects/Point/Program.cs
@@ -0,0 +1,39 @@
+﻿// Cài đặt lớp Point
+class Point
+{
+    // Khai báo thuộc tính (X, Y) là tọa độ điểm trong mặt phẳng
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    // Hàm thiết lập
+    public Point(double x = 0, double y = 0)
+    {
+        X = x; Y = y;
+    }
+
+    // Phương thức nhập
+    public void Nhap()
+    {
+        Console.Write("Nhap hoanh do X");
+        X = double.Parse(Console.ReadLine());
+        Console.Write("Nhap tung do do Y");
+        Y = double.Parse(Console.ReadLine());
+    }
+
+    // Phương thức xuất
+    public void Xuat()
+    {
+        Console.WriteLine("({0},{1})", X, Y);
+    }
+
+}
+
+// Chương trình chính
+class Program
+{
+    public static void Main()
+    {
+        Point p1 = new Point(1, 2);
+        p1.Xuat();
+    }
+}
---
+++ docs/labs/02-classes-and-objects/Point/README.md
@@ -0,0 +1,28 @@
+# Xây dựng lớp Point biểu diễn các điểm trong mặt phẳng
+
+## Mục đích
+
+- Giải quyết bài toán đơn giản theo phương pháp hướng đối tượng.
+
+<img src="img/point2d.png">
+
+## Yêu cầu
+
+### Lớp Point mô tả 1 điểm trong mặt phẳng
+### Các thuộc tính
+- Tọa độ X, Y
+### Các phương thức
+- Thiết lập
+- Nhập
+- Xuất
+
+### Lớp PointList mô tả danh sách các điểm
+#### Các thuộc tính
+- int SoLuong (số điểm)
+- Point[] PList (danh sách điểm)
+
+### Chương trình chính
+
+- Nhập danh sách n điểm trong mặt phẳng.
+- Tìm điểm gần/xa gốc tọa độ nhất.
+- Tìm cặp điểm gần nhau/xa nhau nhất.
