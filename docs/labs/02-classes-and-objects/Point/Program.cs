f75466098c0b9a7a859debf1a90fbaee34b57c08
Hung Nguyen <hungdhnt@gmail.com>
2023-03-15 20:18:41 +0700

    Update

--- docs/labs/02-classes-and-objects/Point/Program.cs
+++ docs/labs/02-classes-and-objects/Point/Program.cs
@@ -3,10 +3,11 @@ class Program
 {
     public static void Main()
     {
-        PointList pl = new PointList();
-        pl.Nhap();
-        pl.Xuat();
-        pl.DiemGanNhat();
-        pl.CapDiemGanNhat();
+        // Tạo danh sách động các đối tượng điểm
+        List<Point> myarray = new List<Point>();
+        myarray.Add(new Point(1, 2));
+        myarray.Add(new Point(3, 4));
+        myarray.Add(new Point(5, 8));
+
     }
 }
