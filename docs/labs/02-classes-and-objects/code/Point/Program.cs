c1c1618b056d86f01548e9393ce772a9ba00a723
nd-hung <hungdhnt@gmail.com>
2023-03-16 09:33:48 +0700

    Update Point example

--- docs/labs/02-classes-and-objects/code/Point/Program.cs
+++ docs/labs/02-classes-and-objects/code/Point/Program.cs
@@ -4,15 +4,16 @@
     {
         // Tạo danh sách động quản lý các đối tượng của lớp Point
         uint nPoints;
-        const int Max=100;
+        const int Max = 100;
         List<Point> pointList = new List<Point>();
 
-        Point GocToaDo = new Point(0,0);
+        Point GocToaDo = new Point(0, 0);
 
-        do{
+        do
+        {
             Console.Write("Nhap so diem = ");
             nPoints = uint.Parse(Console.ReadLine());
-        }while(nPoints <= 0);
+        } while (nPoints <= 0);
 
         // Khởi tạo bộ sinh số ngẫu nhiên
         Random rnd = new Random();
@@ -20,15 +21,25 @@
         {
             // Tạo điểm có tọa độ ngẫu nhiên trong khoảng 100
             pointList.Add(new Point(rnd.Next(Max), rnd.Next(Max)));
-            
+
             // Nếu muốn nhập tọa độ các điểm từ bàn phím thì dùng các lệnh sau
             // pointList.Add(new Point()); // Khởi tạo điểm
             // pointList[i].Nhap();        // Gọi phương thức nhập cho điểm vừa tạo
         }
 
+        // Nếu muốn tạo danh sách đối tượng dạng tĩnh thì dùng các lệnh sau:
+        /*
+        Point[] pList = new Point[nPoints];
+        (int i = 0; i < nPoints; i++)
+        {
+            pList[i] =  new Point();
+            pList[i].Nhap();
+        }
+        */
+
         // In ra các điểm vừa tạo
         Console.WriteLine("Danh sach diem: ");
-        foreach(Point p in pointList)
+        foreach (Point p in pointList)
         {
             p.Xuat();
         }
@@ -36,8 +47,8 @@
         // Tìm điểm gần gốc tọa độ nhất
         double minDistance = pointList[0].Distance(GocToaDo);
         Point nearestPoint = pointList[0];
-        for(int i=1; i<pointList.Count; i++)
-            if(pointList[i].Distance(GocToaDo) < minDistance)
+        for (int i = 1; i < pointList.Count; i++)
+            if (pointList[i].Distance(GocToaDo) < minDistance)
             {
                 minDistance = pointList[i].Distance(GocToaDo);
                 nearestPoint = pointList[i];
