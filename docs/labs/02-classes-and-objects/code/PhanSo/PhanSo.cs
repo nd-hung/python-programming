4b52d68065dfd855d9cb59f6bd078413778c3d47
Hung Nguyen <hungdhnt@gmail.com>
2023-03-17 09:21:53 +0700

    Update

--- docs/labs/02-classes-and-objects/code/PhanSo/PhanSo.cs
+++ docs/labs/02-classes-and-objects/code/PhanSo/PhanSo.cs
@@ -3,14 +3,14 @@
 public class PhanSo
 {
     // Khai báo các thuộc tính
-    public int TuSo{get; set;}
-    public int MauSo{get; set;}
-    
+    public int TuSo { get; set; }
+    public int MauSo { get; set; }
+
     // Phương thức thiết lập
-    public PhanSo(int ts=0, int ms=1)
+    public PhanSo(int ts = 0, int ms = 1)
     {
         TuSo = ts;
-        if(ms != 0) MauSo = ms;
+        if (ms != 0) MauSo = ms;
     }
 
     // Phương thức nhập phân số từ bàn phím
@@ -18,15 +18,45 @@ public class PhanSo
     {
         Console.Write("Tu so = ");
         TuSo = int.Parse(Console.ReadLine());
-        do{
-        Console.Write("Mau so = ");
-        MauSo = int.Parse(Console.ReadLine());
-        }while(MauSo==0);
+        do
+        {
+            Console.Write("Mau so = ");
+            MauSo = int.Parse(Console.ReadLine());
+        } while (MauSo == 0);
     }
 
     // Phương thức in phân số
     public void Xuat()
     {
         Console.WriteLine("{0}/{1}", TuSo, MauSo);
+    }
+
+    // Phương thức tối giản phân số
+    public void ToiGian()
+    {
+        // Tìm UCLN của tử số và mẫu số
+        int ucln = MyMath.UCLN(TuSo, MauSo);
+        TuSo /= ucln;
+        MauSo /= ucln;
+    }
+}
+
+// Lớp chứa thư viện các hàm toán học tự định nghĩa (lớp tĩnh)
+public static class MyMath
+{
+    // Phương thức tìm UCLN của 2 số nguyên dương (phương thức tĩnh)
+    public static int UCLN(int a, int b)
+    {
+        a = Math.Abs(a);
+        b = Math.Abs(b);
+        int min = (a > b) ? b : a;
+        int ucln = 1;
+        for (int i = min; i >= 1; i--)
+            if (a % i == 0 && b % i == 0)
+            {
+                ucln = i;
+                break;
+            }
+        return ucln;
     }
 }
--- docs/labs/02-classes-and-objects/code/PhanSo/Program.cs
+++ docs/labs/02-classes-and-objects/code/PhanSo/Program.cs
@@ -5,9 +5,14 @@ class Program
     static void Main()
     {
         // Tạo phân số bằng cách gọi phương thức thiết lập
-        PhanSo ps1 = new PhanSo(1,2);
+        PhanSo ps1 = new PhanSo(4,12);
         // In phân số
         ps1.Xuat();
+        // Tối giản
+        ps1.ToiGian();
+        // In phân số sau khi tối giản
+        ps1.Xuat();
+
         // Tạo phân số bằng cách gọi phương thức nhập
         PhanSo ps2 = new PhanSo();
         ps2.Nhap();
