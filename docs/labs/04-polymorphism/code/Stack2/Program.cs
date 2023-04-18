f451bf547b95c1594d6c112e52487cf89a18e9d9
Hung Nguyen <hungdhnt@gmail.com>
2023-04-18 08:55:56 +0700

    Update source

--- docs/labs/04-polymorphism/code/Stack2/Program.cs
+++ docs/labs/04-polymorphism/code/Stack2/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     // Hàm PhanTichThua số nhận vào số tự nhiên n 
     // và trả về ngăn xếp chứa các thừa số nguyên tố của n
-    public static PrimeStack PhanTichThuaSo(int n)
+    public static Stack PhanTichThuaSo(int n)
     {
-        PrimeStack stack = new PrimeStack();
+        Stack stack = new PrimeStack();
         for(int i=2; i <=n; i++)
         {
             while(n % i ==0)
@@ -21,9 +21,9 @@ class Program
 
     // Hàm Dec2Hex số nhận vào số tự nhiên n 
     // và trả về ngăn xếp chứa các dư số của n chia cho 16
-    public static HexaStack Dec2Hex(int n)
+    public static Stack Dec2Hex(int n)
     {
-        HexaStack stack = new HexaStack();
+        Stack stack = new HexaStack();
         while(n > 0)
         {
             int soDu = n % 16;
@@ -44,12 +44,12 @@ class Program
         }while(n < 2);
         
         // Phan tich n ra thua so nguyen to
-        PrimeStack stack1 = PhanTichThuaSo(n);
+        Stack stack1 = PhanTichThuaSo(n);
         Console.Write("{0} = ", n);
         stack1.Print();
 
         // Doi n sang he 16
-        HexaStack stack2 = Dec2Hex(n);
+        Stack stack2 = Dec2Hex(n);
         Console.Write("So {0} bieu dien trong he 16 la ", n);
         stack2.Print();
     }
